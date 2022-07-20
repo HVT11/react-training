d10da635196708361f31e31d33cb0fcafd1b8271
HVT11 <huynhtri11102000@gmail.com>
2022-07-20 22:52:36 +0700

    Example for Forms: Reservation

---
+++ my-app/src/component/Form/Reservation.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+class Reservation extends React.Component {
+    state: Readonly<{
+        isGoing: boolean,
+        numberOfGuests: number
+    }>;
+    constructor(props: {}) {
+      super(props);
+      this.state = {
+        isGoing: true,
+        numberOfGuests: 2
+      };
+    }
+  
+    handleInputChange = (event: React.FormEvent<HTMLInputElement>) => {
+      const target = event.currentTarget;
+      const value = target.type === 'checkbox' ? target.checked : target.value;
+      const name = target.name;
+  
+      this.setState({
+        [name]: value
+      });
+    }
+  
+    render() {
+      return (
+        <form>
+          <label>
+            Is going:
+            <input name="isGoing" type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Number of guests:
+            <input name="numberOfGuests" type="number" value={this.state.numberOfGuests} onChange={this.handleInputChange} />
+          </label>
+        </form>
+      );
+    }
+}
+
+export default Reservation
