57933b90e1880b4d0f31fb17cf7ecd288ccccdc4
TriHuynh <huynhtri11102000@gmail.com>
2022-08-01 14:41:27 +0700

    Update search component: props

--- practice-one/src/components/Search/Search.tsx
+++ practice-one/src/components/Search/Search.tsx
@@ -5,12 +5,16 @@ import React from 'react';
 import IconButton from '../Button/IconButton/IconButton';
 import InputSearch from '../Input/InputSearch/InputSearch';
 
-class Search extends React.Component {
+interface ISearch {
+  onClick: () => void
+}
+
+class Search extends React.Component<ISearch> {
   render() {
     return (
       <div className="search" id="search">
-        <InputSearch placeholder="Search user..." />
-        <IconButton icon={['fas', 'xmark']} />
+        <InputSearch />
+        <IconButton icon={['fas', 'xmark']} onClick={this.props.onClick} />
       </div>
     );
   }
