b13ffa5f65f7e1272ffe86a4937f421b2047c210
TriHuynh <huynhtri11102000@gmail.com>
2022-08-08 16:50:26 +0700

    Restructure input component

--- practice-one/src/components/Input/InputText/InputText.tsx
+++ practice-one/src/components/Input/InputText/InputText.tsx
@@ -2,36 +2,45 @@ import './input-text.scss';
 
 import React from 'react';
 
-interface IInputTextProps {
+interface IInputProps {
   name: string;
-  text: string;
+  value?: string;
+  type: 'text' | 'file';
 }
 
 type State = {
-  text: string;
+  value?: string;
 };
 
-class InputText extends React.Component<IInputTextProps, State> {
-  state = { text: this.props.text };
+class Input extends React.Component<IInputProps, State> {
+  state = { value: this.props.value ? this.props.value : '' };
 
   handleChangeText = (event: React.FormEvent<HTMLInputElement>) => {
-    this.setState({ text: event.currentTarget.value });
+    this.setState({ value: event.currentTarget.value });
   };
+
+  static getDerivedStateFromProps(props: IInputProps, state: State) {
+    if (props.value !== state.value) {
+      return {
+        value: props.value,
+      };
+    } else return null;
+  }
 
   render() {
     return (
       <div className="input-box">
         <p className="input-box__label">{this.props.name}:</p>
         <input
-          type="text"
+          type={this.props.type}
           className="input-box__input"
           id={this.props.name}
           onChange={this.handleChangeText}
-          value={this.state.text}
+          value={this.state.value}
         />
       </div>
     );
   }
 }
 
-export default InputText;
+export default Input;
