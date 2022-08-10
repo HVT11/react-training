c454b6ed822c265864a69804af27c8bf53d50695
TriHuynh <huynhtri11102000@gmail.com>
2022-08-10 20:14:12 +0700

    Restructe modal component: add style for modal__input

--- practice-one/src/components/Modal/Modal.tsx
+++ practice-one/src/components/Modal/Modal.tsx
@@ -8,20 +8,30 @@ import Button from '../Button/NormalButton/Button';
 interface IModal {
   stateModal: boolean;
   title: string;
-  nameBtn1: string;
-  onClickBtn1: () => void;
-  value: string;
-  nameBtn2?: string;
-  onClickBtn2?: () => void;
   hasInput?: boolean;
-  // eslint-disable-next-line no-unused-vars
+  value: string;
+  nameBtnPrimary: string;
+  nameBtnSecondary?: string;
+  onClickBtnPrimary: () => void;
+  onClickBtnSecondary?: () => void;
+  onhandleCloseModal: () => void;
   onHandleChange: (event: React.FormEvent<HTMLInputElement>) => void;
-  handleCloseModal: () => void;
 }
 
 class Modal extends React.Component<IModal> {
   render() {
-    const { title, hasInput, nameBtn1, nameBtn2, onClickBtn1, onClickBtn2 } = this.props;
+    const {
+      title,
+      hasInput,
+      value,
+      nameBtnPrimary,
+      nameBtnSecondary,
+      onClickBtnPrimary,
+      onClickBtnSecondary,
+      onhandleCloseModal,
+      onHandleChange,
+    } = this.props;
+
     return (
       this.props.stateModal && (
         <div className="modal">
@@ -29,18 +39,31 @@ class Modal extends React.Component<IModal> {
           <div className="modal__body">
             <div className="modal__header">
               <h3 className="modal__title">{title}</h3>
-              <IconButton icon={['fas', 'xmark']} onClick={this.props.handleCloseModal} />
+              <IconButton icon={['fas', 'xmark']} onHandleClick={onhandleCloseModal} />
             </div>
             <div className="d-flex d-flex--space-between">
               {hasInput && (
                 <input
                   type="text"
-                  value={this.props.value}
-                  onChange={this.props.onHandleChange}
+                  className="modal__input"
+                  value={value}
+                  onChange={onHandleChange}
                 />
               )}
-              {nameBtn1 && <Button label={nameBtn1} size="small" onClick={onClickBtn1} />}
-              {nameBtn2 && <Button label={nameBtn2} size="small" onClick={onClickBtn2} />}
+              {nameBtnPrimary && (
+                <Button
+                  label={nameBtnPrimary}
+                  size="small"
+                  onHandleClick={onClickBtnPrimary}
+                />
+              )}
+              {nameBtnSecondary && (
+                <Button
+                  label={nameBtnSecondary}
+                  size="small"
+                  onHandleClick={onClickBtnSecondary}
+                />
+              )}
             </div>
           </div>
         </div>
