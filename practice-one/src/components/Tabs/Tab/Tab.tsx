8a31d3b470c13c7173f14f5ad22acb3dd8d6742f
TriHuynh <huynhtri11102000@gmail.com>
2022-08-11 11:47:48 +0700

    Restructe tabs components

--- practice-one/src/components/Tabs/Tab/Tab.tsx
+++ practice-one/src/components/Tabs/Tab/Tab.tsx
@@ -1,26 +1,27 @@
 import '../tabs.scss';
 
+import clsx from 'clsx';
 import React from 'react';
 
 interface ITabProps {
   id: string;
   label: string;
   index?: string;
-  // eslint-disable-next-line no-unused-vars
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 class Tab extends React.Component<ITabProps> {
   render() {
-    const active = this.props.index === this.props.id ? 'tab--active' : '';
+    const { id, index, label, onClick } = this.props;
+
     return (
       <button
         role="tab"
-        id={`${this.props.id}`}
-        className={['tab', active].join(' ')}
-        onClick={this.props.onClick}
+        id={id}
+        className={clsx('tab', { 'tab--active': index === id })}
+        onClick={onClick}
       >
-        {this.props.label}
+        {label}
       </button>
     );
   }
