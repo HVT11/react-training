0a06687bde71525d155e6052e2cb6934c1694c1b
HVT11 <huynhtri11102000@gmail.com>
2022-08-12 17:34:44 +0700

    Fix format user

--- practice-one/src/utils/format/dataFormat.ts
+++ practice-one/src/utils/format/dataFormat.ts
@@ -1,11 +1,14 @@
 import { IUser } from '../interface/IUser';
 
 export const dataFormat = (data: IUser[]) => {
-  return data.map((user) => ({
-    id: user.id.toString(),
-    username: user.name,
-    status: user.status ? true : false,
-    email: user.email,
-    url: user.avatar,
-  }));
+  return data.map((user) => (
+    {user: {
+        id: user.id.toString(),
+        username: user.name,
+        status: user.status ? true : false,
+        email: user.email,
+        url: user.avatar,
+      }
+    }
+  ));
 };
