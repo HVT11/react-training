265b2eb66774fd267632ad4ed8641516d91f3bcc
TriHuynh <huynhtri11102000@gmail.com>
2022-08-16 10:50:54 +0700

    Create folder cointainer and move folder roles, rules, user to this folder

---
+++ practice-one/src/container/Rules/Rules.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import GridColumn from '../../layout/Grid/GridColumn/GridColumn';
+import GridRow from '../../layout/Grid/GridRow/GridRow';
+
+class RuleManager extends React.Component {
+  render() {
+    return (
+      <GridRow>
+        <GridColumn size="xl">
+          <h2>This is rules manager</h2>
+        </GridColumn>
+      </GridRow>
+    );
+  }
+}
+
+export default RuleManager;
